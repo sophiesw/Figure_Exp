b6888704c961a8255ba1cc6da38958fe9d6e6506
sophiesw <sophiesw@hotmail.com>
2014-10-11 02:09:44 -0700

    Course Project 1
    Course Project 1 for Exploratory Data Analysis

---
+++ plot1.R
@@ -0,0 +1,17 @@
+data <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings = "NA", colClasses = "character")
+data$Date <- as.Date(data$Date,"%d/%m/%Y")
+sdata <- subset(data, as.Date(Date) >= '2007-02-01' & as.Date(Date) <= '2007-02-02')
+sdata$E_Time = paste(sdata$Date, sdata$Time, sep=" ")
+sdata$E_Time <- strptime(sdata$E_Time, "%Y-%m-%d %H:%M:%S")
+
+sdata$Voltage <- as.numeric(sdata$Voltage)
+sdata$Global_active_power <- as.numeric(sdata$Global_active_power)
+sdata$Global_reactive_power <- as.numeric(sdata$Global_reactive_power)
+sdata$Global_intensity <- as.numeric(sdata$Global_intensity)
+sdata$Sub_metering_1  <- as.numeric(sdata$Sub_metering_1 )
+sdata$Sub_metering_2  <- as.numeric(sdata$Sub_metering_2)
+sdata$Sub_metering_3  <- as.numeric(sdata$Sub_metering_3)
+
+png("plot1.png", width = 480, height = 480)
+hist(sdata$Global_active_power, col="red", main= "Global Active Power", xlab ="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,27 @@
+data <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings = "NA", colClasses = "character")
+data$Date <- as.Date(data$Date,"%d/%m/%Y")
+sdata <- subset(data, as.Date(Date) >= '2007-02-01' & as.Date(Date) <= '2007-02-02')
+sdata$E_Time = paste(sdata$Date, sdata$Time, sep=" ")
+sdata$E_Time <- strptime(sdata$E_Time, "%Y-%m-%d %H:%M:%S")
+
+sdata$Voltage <- as.numeric(sdata$Voltage)
+sdata$Global_active_power <- as.numeric(sdata$Global_active_power)
+sdata$Global_reactive_power <- as.numeric(sdata$Global_reactive_power)
+sdata$Global_intensity <- as.numeric(sdata$Global_intensity)
+sdata$Sub_metering_1  <- as.numeric(sdata$Sub_metering_1 )
+sdata$Sub_metering_2  <- as.numeric(sdata$Sub_metering_2)
+sdata$Sub_metering_3  <- as.numeric(sdata$Sub_metering_3)
+
+png("plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2), mar= c(5, 5, 2, 2), oma = c(0.5, 0.5, 0.5, 0.5), mgp = c(3, 1, 0), cex=0.8)
+plot(sdata$E_Time, sdata$Global_active_power, ylab ="Global Active Power", main="", xlab="", sub="", type= "l")
+plot(sdata$E_Time, sdata$Voltage, ylab ="Voltage", main="", xlab="datetime", sub="", type= "l")
+with(sdata, {
+  plot(E_Time, Sub_metering_1, col ="black", type="l",
+       ylab ="Global Active Power", main="", xlab="", sub="")
+  lines(E_Time, Sub_metering_2, col ="red")
+  lines(E_Time, Sub_metering_3, col ="blue")
+})
+legend("topright",bty="n", cex= 0.9, lty=c(1,1,1), col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+plot(sdata$E_Time, sdata$Global_reactive_power, ylab ="Global_reactive_power", main="", xlab="datetime", sub="", type= "l")
+dev.off()
